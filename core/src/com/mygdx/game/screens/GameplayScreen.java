73034415296a217d8f851e00162c2a98e71adb6f
bryant231 <bryant23195@gmail.com>
2016-10-17 17:43:44 +0200

    Dodanie gameplay screen.

---
+++ core/src/com/mygdx/game/screens/GameplayScreen.java
@@ -0,0 +1,16 @@
+package com.mygdx.game.screens;
+
+import com.mygdx.game.Gra_testowa;
+
+public class GameplayScreen extends AbstractScreen {
+
+	public GameplayScreen(Gra_testowa game) {
+		super(game);
+		init();
+	}
+
+	private void init() {
+
+	}
+
+}
